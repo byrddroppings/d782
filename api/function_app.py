94a20542318c6e8998f85010d487e8676ecaa726
Ken Byrd <ken@byrddroppings.net>
2025-11-06 15:17:23 -0600

    fix function permissions

--- api/function_app.py
+++ api/function_app.py
@@ -4,19 +4,21 @@ import json
 import logging
 import os
 
+from urllib.parse import quote
+
 from azure.storage.blob import (
     generate_blob_sas, BlobSasPermissions
 )
 
 app = func.FunctionApp()
 
-@app.route(route="video-sas", auth_level=func.AuthLevel.ADMIN)
+@app.route(route="video-sas", auth_level=func.AuthLevel.ANONYMOUS)
 def videoSas(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     blob = req.params.get('blob')
     if not blob:
-        return func.HttpResponse("No blob specified.", status_code=401)
+        return func.HttpResponse("No blob specified.", status_code=400)
 
     account = os.environ["STORAGE_ACCOUNT_NAME"]
     key = os.environ["STORAGE_ACCOUNT_KEY"]
@@ -35,7 +37,7 @@ def videoSas(req: func.HttpRequest) -> func.HttpResponse:
         protocol="https"
     )
 
-    url = f"https://{account}.blob.core.windows.net/{container}/{blob}?{sas}"
+    url = f"https://{account}.blob.core.windows.net/{container}/{quote(blob, safe='/')}?{sas}"
     
     return func.HttpResponse(
         json.dumps({"url": url}), status_code=200,
@@ -43,18 +45,18 @@ def videoSas(req: func.HttpRequest) -> func.HttpResponse:
     )
     
 
-@app.route(route="video", auth_level=func.AuthLevel.ADMIN)
+@app.route(route="video", auth_level=func.AuthLevel.ANONYMOUS)
 def video(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     blob = req.params.get('blob')
     if not blob:
-        return func.HttpResponse("No blob specified.", status_code=401)
+        return func.HttpResponse("No blob specified.", status_code=400)
 
     account = os.environ["STORAGE_ACCOUNT_NAME"]
     container = os.environ.get("VIDEO_CONTAINER", "videos")
 
-    url = f"https://{account}.blob.core.windows.net/{container}/{blob}"
+    url = f"https://{account}.blob.core.windows.net/{container}/{quote(blob, safe='/')}"
     
     return func.HttpResponse(
         json.dumps({"url": url}), status_code=200,
